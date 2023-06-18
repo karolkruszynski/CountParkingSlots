510ccc31f5ff09d8b2e1b901b9c1a1e85f8391b0
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-06-18 18:24:26 +0200

    import csv and math lib mod: turtle spee add: csv reader and paint the middle parking slots with green dots reader to read specify row which user gave, and then make the dot red to claim the space make a simply infity loop to add another claims for spots

--- venv/main.py
+++ venv/main.py
@@ -1,5 +1,6 @@
 import turtle
 import csv
+import math
 
 # Screen Set Up
 
@@ -9,6 +10,38 @@ sc.title("Count Parking Slots")
 image = "park-parking-lot-plan.gif"
 sc.setup(650,397)
 sc.addshape(image)
-tr.shape(image)
+sc.bgpic("park-parking-lot-plan.gif")
+tr.speed("fastest")
 
+
+with open("CPS_Middle_Parking_Slot_Point.csv", "r") as mid_slot_csv:
+    reader = csv.reader(mid_slot_csv)
+    for row in reader:
+        print(row)
+        tr.penup()
+        tr.goto(float(row[0]),float(row[1]))
+        tr.dot(10,"green")
+
+
+def read_specific_row(user_input):
+    with open("CPS_Middle_Parking_Slot_Point.csv", "r") as mid_slot_csv:
+        reader = csv.reader(mid_slot_csv)
+        for index, row in enumerate(reader):
+            if index + 1 == user_input:
+                return row
+
+    return None
+
+
+def claim_parking_slot(coor_x,coor_y):
+    tr.penup()
+    tr.goto(coor_x,coor_y)
+    tr.dot(10,"red")
+
+while True:
+    user_input = sc.numinput("Poker", "Your stakes:", 1000, minval=10, maxval=10000)
+    coor = read_specific_row(user_input)
+    coor_x = float(coor[0])
+    coor_y = float(coor[1])
+    claim_parking_slot(coor_x,coor_y)
 sc.mainloop()
